3ba2b373d953c4bc7b5f29c754e22060e323f27e
Nathan Gerday <nat.gerday@gmail.com>
2020-02-03 01:30:22 +0100

    Better theme + icons

--- app/src/main/java/com/example/mylife/MainActivity.java
+++ app/src/main/java/com/example/mylife/MainActivity.java
@@ -79,13 +79,6 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
         switch(menuItem.getItemId()){
             case R.id.nav_lists:
